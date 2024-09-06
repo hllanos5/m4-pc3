77413f282f75eb122ba7399da51aac467e5ba5a0
Hans Llanos <hans.llanos@gmail.com>
2024-09-05 23:20:43 -0500

    practica terminada

--- practica/src/pages/Crew.jsx
+++ practica/src/pages/Crew.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { Image } from 'primereact/image';
 
 export default function Crew() {
   return (
     <main>
-        <NavBar/>
-        <div>
-          Crew
+        <NavBar/>image-anousheh-ansari
+        <div className='contenido' >
+          <div className='lado-izquierdo' style={{textAlign:"right"}}>
+            <h2>01 PICK YOUR DESTINATION</h2>
+            <Image src="./assets/destination/image-mars.png" alt="Image" width="350" />
+          </div>
+          <div className="lado-derecho">
+            <Image src="./assets/crew/image-anousheh-ansari.png" alt="Image" width="350" />
+          </div>
         </div>
     </main>
   )
--- practica/src/pages/Technology.jsx
+++ practica/src/pages/Technology.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { Image } from 'primereact/image';
 
 export default function Technology() {
   return (
     <main>
         <NavBar/>
-        <div>
-          Technology
+        <div className='contenido' >
+          <div className='lado-izquierdo' style={{textAlign:"right"}}>
+            <h2>01 PICK YOUR DESTINATION</h2>
+            <Image src="./assets/destination/image-mars.png" alt="Image" width="350" />
+          </div>
+          <div className="lado-derecho">
+            <Image src="./assets/technology/image-launch-vehicle-portrait.jpg" alt="Image" width="350" />
+          </div>
         </div>
     </main>
   )
