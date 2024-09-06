d278759b90aa6de2f28e657d27ceee1c22cdf720
Hans Llanos <hans.llanos@gmail.com>
2024-09-05 22:32:20 -0500

    implementacion de tab2

--- practica/src/pages/Destination.jsx
+++ practica/src/pages/Destination.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { Image } from 'primereact/image';
+import { TabMenu } from 'primereact/tabmenu';
 
 export default function Destination() {
+  const items = [
+    { label: 'MOON', },
+    { label: 'MARS', },
+    { label: 'EUROPA', },
+    { label: 'TITAN', }
+];
+  
   return (
     <main>
         <NavBar/>
-        <div>
-          Destination
+        <div className='contenido' >
+          <div className='lado-izquierdo' style={{textAlign:"right"}}>
+            <h2>01 PICK YOUR DESTINATION</h2>
+            <Image src="./assets/destination/image-mars.png" alt="Image" width="350" />
+          </div>
+          <div className="lado-derecho">
+            <TabMenu model={items} />
+          </div>
         </div>
     </main>
   )
