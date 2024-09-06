e60ad1cc725382bcb8e0846cc5c822fe0bab4e98
Hans Llanos <hans.llanos@gmail.com>
2024-09-05 21:38:29 -0500

    implementacion de home

--- practica/src/pages/Home.jsx
+++ practica/src/pages/Home.jsx
@@ -5,8 +5,19 @@ export default function Home() {
   return (
     <main>
         <NavBar/>
-        <div>
-          Home
+        <div className='contenido'>
+          <div className='lado-izquierdo'>
+            <label>So, you want to travel to</label>
+            <h1>Space</h1>
+            <p>
+            Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+            </p>
+          </div>
+          <div className="lado-derecho">
+            <div className='explorer'>
+              EXPLORER
+            </div>
+          </div>
         </div>
     </main>
   )
